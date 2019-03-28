03a67e5e7f4d2e9d73487278371d652fd16b98b7
eraserrain1 <michaeltan32@gmail.com>
2019-03-28 15:36:32 -0400

    converted bot to client convention. Revised Comments.

--- index.js
+++ index.js
@@ -12,25 +12,26 @@ const Discord = require('discord.js');
 const prefix = botconfig.prefix;
 const token = botconfig.token;
 
+// Checks if token is available
 if (token.length < 1)
 {
     console.log('token is not configured.');
     process.exit();
 }
 
-const bot = new Discord.Client({disableEveryone: true});
+const client = new Discord.Client({disableEveryone: true});
 
 // Commands
 const pingPong = require('./commands/pingpong');
 
 // Bot turns on
-bot.on('ready', async () => {
-    console.log(`${bot.user.tag} is online!`);
-    bot.user.setPresence({ game: { name: 'Your Heart! ♥ ツ' } });
+client.on('ready', async () => {
+    console.log(`${client.user.tag} is online!`);
+    client.user.setPresence({ game: { name: 'Your Heart! ♥ ツ' } });
 });
 
 // Bot trigggers on message
-bot.on('message', msg => {
+client.on('message', msg => {
     // Ignores messages if bot is the one that sends them.
     if(msg.author.bot != true)
     {
@@ -49,7 +50,6 @@ bot.on('message', msg => {
                     default:
                         msg.reply("No command found, please try again.");
                         break;
-
                 }
             }
         }
@@ -60,34 +60,5 @@ bot.on('message', msg => {
     }
 });
 
-
-/*
-bot.on('message', async msg => {
-    console.log(`message detetected`);
-
-    if (msg.content.mentions.Client) {
-        msg.reply(`${prefix}help for commands.`)
-    }
-
-    if (msg.content.toLowerCase() === `${prefix}invitethebottoserver`) {
-        msg.reply(`This is the invite for the bot: https://discordapp.com/api/oauth2/authorize?client_id=${botconfig.clientID}&permissions=0&scope=bot`);
-    }
-
-    if (msg.content.toLowerCase() === `${prefix}ping`) {
-        console.log(`${bot.user.username} message reply Pong!`);
-        msg.channel.send('Pong!');
-    }
-
-    if (msg.content.toLowerCase() === `${prefix}avatar`){
-        console.log(`${bot.user.username} avatar command.`);
-        msg.reply(msg.author.avatarURL);
-    }
-    
-    if (msg.content.toLowerCase() === `${prefix}avatar ${msg.mentions.members}`){
-        console.log(`${bot.user.username} avatar command mention.`);
-        msg.reply(msg.members.first().avatarURL);
-    }
-})
-*/
-
-bot.login(token);
+// Login bot with token.
+client.login(token);
